2bb4693f5e4281205c93498965e9b425392f70a3
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-09-22 21:21:18 +0200

    prefer violin plots

--- R/21_Plot_flood_protection.R
+++ R/21_Plot_flood_protection.R
@@ -1,18 +1,10 @@
 # ----------------------
 # A) Infiltration
 # ----------------------
-m_infil <- lmer(log1p(infiltration_adjusted) ~ sample_place + (1 | site_id),
-                data = data_raw)
-m_infil_null <- lmer(log1p(infiltration_adjusted) ~ 1 + (1 | site_id),
-                     data = data_raw)
-
-anova(m_infil, m_infil_null)
-AIC(m_infil, m_infil_null)
-
 pred_infil <- ggpredict(m_infil, terms = "sample_place")
 
 p_infil <- ggplot(pred_infil, aes(x, predicted)) +
-  geom_col(fill = "skyblue") +
+  geom_violin(fill = "skyblue") +
   geom_errorbar(aes(ymin = conf.low, ymax = conf.high), width = 0.2) +
   labs(x = "Habitat (sample_place)",
        y = "log(1+Infiltration)",
@@ -22,19 +14,10 @@ p_infil <- ggplot(pred_infil, aes(x, predicted)) +
 # ----------------------
 # B) Water field capacity
 # ----------------------
-m_wfc <- lmer(WFC_adjusted ~ sample_place * depth_cm +
-                infiltration_adjusted + AWS + (1 | site_id),
-              data = data_raw)
-m_wfc_null <- lmer(WFC_adjusted ~ 1 + (1 | site_id),
-                   data = data_raw)
-
-anova(m_wfc, m_wfc_null)
-AIC(m_wfc, m_wfc_null)
-
 pred_wfc <- ggpredict(m_wfc, terms = c("sample_place", "depth_cm"))
 
 p_wfc <- ggplot(pred_wfc, aes(x = x, y = predicted, colour = group)) +
-  geom_line(size = 1) +
+  geom_point() +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group),
               alpha = 0.2, colour = NA) +
   labs(x = "Habitat (sample_place)",
