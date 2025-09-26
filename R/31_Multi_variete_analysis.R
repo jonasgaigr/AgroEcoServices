1a5486ffb797894540fb42c170bb9f5f99a3200f
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-09-26 20:27:12 +0200

    add arrows for variables

--- R/31_Multi_variete_analysis.R
+++ R/31_Multi_variete_analysis.R
@@ -5,11 +5,15 @@ soil_vars <- data_raw %>%
     WFC_adjusted,
     AWS,
     SOC,
-    BS
+    BS,
+    WFC_not_adjusted
     )
 
 # --- PCA (unconstrained ordination) ---
 pca <- rda(soil_vars, scale = TRUE)
+
+# Fit soil variables as vectors (arrows)
+ef_pca <- envfit(pca, soil_vars, permutations = 999)
 
 # Summary of PCA
 summary(pca)
@@ -22,30 +26,40 @@ site_scores <- scores(pca, display = "sites", scaling = 2)
 site_scores_df <- as.data.frame(site_scores)
 site_scores_df$sample_place <- data_raw$sample_place
 
+# Arrows (biplot scores for soil variables)
+arrow_scores <- as.data.frame(scores(ef_pca, display = "vectors", scaling = 2))
+
 # Compute centroids
 centroids <- aggregate(cbind(PC1 = site_scores_df$PC1, PC2 = site_scores_df$PC2),
                        by = list(sample_place = site_scores_df$sample_place),
                        FUN = mean)
 
-# PCA plot with hulls and labels
-pca <- ggplot(site_scores_df, aes(x = PC1, y = PC2, color = sample_place)) +
+# PCA plot with hulls, centroids, and arrows
+pca_plot <- ggplot(site_scores_df, aes(x = PC1, y = PC2, color = sample_place)) +
   geom_point() +
-  ggpubr::stat_chull(aes(group = sample_place), alpha = 0.2, geom = "polygon") +
-  ggrepel::geom_text_repel(data = centroids,
+  stat_chull(aes(group = sample_place), alpha = 0.2, geom = "polygon") +
+  geom_segment(data = arrow_scores,
+               aes(x = 0, y = 0, xend = PC1, yend = PC2),
+               arrow = arrow(length = unit(0.2, "cm")),
+               inherit.aes = FALSE,
+               color = "red") +
+  geom_text_repel(data = centroids,
                   aes(x = PC1, y = PC2, label = sample_place),
                   size = 5, fontface = "bold", color = "black") +
+  geom_text_repel(data = arrow_scores,
+                  aes(x = PC1, y = PC2, label = rownames(arrow_scores)),
+                  inherit.aes = FALSE,
+                  color = "red", size = 4) +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5)) +
   ggtitle("PCA (correlation biplot)\nSoil variables")
 
-ggsave(pca, filename = "Outputs/Plots/pca.png",
-       height = 5, width = 6)
-
+ggsave(pca_plot, filename = "Outputs/Plots/pca.png", height = 5, width = 6)
 
 # --- RDA (constrained ordination) ---
 # Explanatory variables: sample_place + depth_cm + texture
 rda_model <- rda(
-  soil_vars ~ sample_place + depth_cm + texture, 
+  soil_vars ~ sample_place + texture, 
   data = data_raw, 
   scale = TRUE
   )
@@ -58,20 +72,28 @@ anova(rda_model)                 # overall test
 anova(rda_model, by = "axis")    # by axis
 anova(rda_model, by = "terms")   # by terms
 
-# Extract RDA site scores
+# Site scores
 site_scores <- scores(rda_model, display = "sites", scaling = 2)
 site_scores_df <- as.data.frame(site_scores)
 site_scores_df$sample_place <- data_raw$sample_place
 
-# Compute centroids for groups
+# Arrows (biplot scores for soil variables)
+arrow_scores_rda <- as.data.frame(scores(rda_model, display = "bp", scaling = 2))
+
+# Compute centroids
 centroids <- aggregate(cbind(RDA1 = site_scores_df$RDA1, RDA2 = site_scores_df$RDA2),
                        by = list(sample_place = site_scores_df$sample_place),
                        FUN = mean)
 
-# RDA plot with hulls and centroid labels
-rda <- ggplot(site_scores_df, aes(x = RDA1, y = RDA2, color = sample_place)) +
+# RDA plot with hulls, centroids, and arrows
+rda_plot <- ggplot(site_scores_df, aes(x = RDA1, y = RDA2, color = sample_place)) +
   geom_point(size = 2) +
   stat_chull(aes(group = sample_place), alpha = 0.2, geom = "polygon") +
+  geom_segment(data = arrow_scores_rda,
+               aes(x = 0, y = 0, xend = RDA1, yend = RDA2),
+               arrow = arrow(length = unit(0.2, "cm")),
+               inherit.aes = FALSE,
+               color = "red") +
   geom_text_repel(data = centroids,
                   aes(x = RDA1, y = RDA2, label = sample_place),
                   size = 5, fontface = "bold", color = "black") +
@@ -80,5 +102,4 @@ rda <- ggplot(site_scores_df, aes(x = RDA1, y = RDA2, color = sample_place)) +
   theme(plot.title = element_text(hjust = 0.5)) +
   labs(x = "RDA1", y = "RDA2")
 
-ggsave(rda, filename = "Outputs/Plots/rda.png",
-       height = 5, width = 6)
+ggsave(rda_plot, filename = "Outputs/Plots/rda.png", height = 5, width = 6)
