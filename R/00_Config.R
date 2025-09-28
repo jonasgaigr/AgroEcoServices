3caa624dd2a4d13db1685fc846cbdb8a79e2e3d2
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-09-28 17:29:30 +0200

    correct depth categories

--- R/00_Config.R
+++ R/00_Config.R
@@ -66,5 +66,5 @@ habitat_cols <- c(
 depth_cols <- c(
   "0-5"   = "#a6cee3",  # light blue
   "10-15" = "#e07b39",  # ochre/orange
-  "25-50" = "#4b2e2e"   # dark brown
+  "25-30" = "#4b2e2e"   # dark brown
 )
