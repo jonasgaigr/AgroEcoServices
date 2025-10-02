8e8db1f6f206def4948d3baa8b1ba65838569365
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-10-02 11:08:55 +0200

    adjust models

--- R/12_Analysis_sites.R
+++ R/12_Analysis_sites.R
@@ -22,7 +22,6 @@ m_site_infil_null <- stats::glm(
 stats::anova(m_site_infil, m_site_infil_null, test = "Chisq")
 stats::AIC(m_site_infil, m_site_infil_null)
 
-graphics::plot(m_site_infil)                             # diagnostic plots for GLM
 stats::qqnorm(stats::resid(m_site_infil)); stats::qqline(stats::resid(m_site_infil))
 
 # tidy & save
@@ -36,6 +35,8 @@ write_tab_site_infil <- sjPlot::tab_model(
   dv.labels = c("log(1+Infiltration) ~ Site", "Null model"),
   file = "Outputs/Tables/Infiltration_site_models.doc"
 )
+
+write_tab_site_infil
 
 report_site_infil <- report::report(m_site_infil)
 base::cat(report_site_infil, file = "Outputs/Tables/Infiltration_site_report.txt")
@@ -58,7 +59,6 @@ m_site_wfc_null <- stats::glm(
 stats::anova(m_site_wfc, m_site_wfc_null, test = "Chisq")
 stats::AIC(m_site_wfc, m_site_wfc_null)
 
-graphics::plot(m_site_wfc)
 stats::qqnorm(stats::resid(m_site_wfc)); stats::qqline(stats::resid(m_site_wfc))
 
 tab_site_wfc <- broom::tidy(m_site_wfc, conf.int = TRUE, conf.level = 0.95)
@@ -72,20 +72,22 @@ write_tab_site_wfc <- sjPlot::tab_model(
   file = "Outputs/Tables/WFC_site_models.doc"
 )
 
+write_tab_site_wfc
+
 report_site_wfc <- report::report(m_site_wfc)
 base::cat(report_site_wfc, file = "Outputs/Tables/WFC_site_report.txt")
 
 # ----------------------
 # C) Aggregate water stability (AWS) (site effect) ----
 # ----------------------
-m_site_aws <- stats::glm(
-  AWS ~ site_id,
+m_site_aws <- glm(
+  log1p(AWS) ~ site_id,
   data = data_raw,
-  family = stats::gaussian()
+  family = gaussian()
 )
 
 m_site_aws_null <- stats::glm(
-  AWS ~ 1,
+  log1p(AWS) ~ 1,
   data = data_raw,
   family = stats::gaussian()
 )
@@ -93,7 +95,6 @@ m_site_aws_null <- stats::glm(
 stats::anova(m_site_aws, m_site_aws_null, test = "Chisq")
 stats::AIC(m_site_aws, m_site_aws_null)
 
-graphics::plot(m_site_aws)
 stats::qqnorm(stats::resid(m_site_aws)); stats::qqline(stats::resid(m_site_aws))
 
 tab_site_aws <- broom::tidy(m_site_aws, conf.int = TRUE, conf.level = 0.95)
@@ -107,6 +108,8 @@ write_tab_site_aws <- sjPlot::tab_model(
   file = "Outputs/Tables/AWS_site_models.doc"
 )
 
+write_tab_site_aws
+
 report_site_aws <- report::report(m_site_aws)
 base::cat(report_site_aws, file = "Outputs/Tables/AWS_site_report.txt")
 
@@ -116,19 +119,18 @@ base::cat(report_site_aws, file = "Outputs/Tables/AWS_site_report.txt")
 m_site_soc <- stats::glm(
   log1p(SOC) ~ site_id,
   data = data_raw,
-  family = stats::gaussian()
+  family = stats::Gamma(link = "log")
 )
 
 m_site_soc_null <- stats::glm(
   log1p(SOC) ~ 1,
   data = data_raw,
-  family = stats::gaussian()
+  family = stats::Gamma(link = "log")
 )
 
 stats::anova(m_site_soc, m_site_soc_null, test = "Chisq")
 stats::AIC(m_site_soc, m_site_soc_null)
 
-graphics::plot(m_site_soc)
 stats::qqnorm(stats::resid(m_site_soc)); stats::qqline(stats::resid(m_site_soc))
 
 tab_site_soc <- broom::tidy(m_site_soc, conf.int = TRUE, conf.level = 0.95)
@@ -142,6 +144,8 @@ write_tab_site_soc <- sjPlot::tab_model(
   file = "Outputs/Tables/SOC_site_models.doc"
 )
 
+write_tab_site_soc
+
 report_site_soc <- report::report(m_site_soc)
 base::cat(report_site_soc, file = "Outputs/Tables/SOC_site_report.txt")
 
@@ -149,13 +153,13 @@ base::cat(report_site_soc, file = "Outputs/Tables/SOC_site_report.txt")
 # E) Bare Soil (BS) (site effect) ----
 # ----------------------
 m_site_bs <- stats::glm(
-  BS ~ site_id,
+  log1p(BS) ~ site_id,
   data = data_raw,
   family = stats::gaussian()
 )
 
 m_site_bs_null <- stats::glm(
-  BS ~ 1,
+  log1p(BS) ~ 1,
   data = data_raw,
   family = stats::gaussian()
 )
@@ -163,7 +167,6 @@ m_site_bs_null <- stats::glm(
 stats::anova(m_site_bs, m_site_bs_null, test = "Chisq")
 stats::AIC(m_site_bs, m_site_bs_null)
 
-graphics::plot(m_site_bs)
 stats::qqnorm(stats::resid(m_site_bs)); stats::qqline(stats::resid(m_site_bs))
 
 tab_site_bs <- broom::tidy(m_site_bs, conf.int = TRUE, conf.level = 0.95)
@@ -176,6 +179,8 @@ write_tab_site_bs <- sjPlot::tab_model(
   dv.labels = c("BS ~ Site", "Null model"),
   file = "Outputs/Tables/BS_site_models.doc"
 )
+
+write_tab_site_bs
 
 report_site_bs <- report::report(m_site_bs)
 base::cat(report_site_bs, file = "Outputs/Tables/BS_site_report.txt")
