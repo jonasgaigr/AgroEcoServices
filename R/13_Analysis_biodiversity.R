49fac5f29af26cb89c94a8fb478ee6a20e75194a
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-10-04 14:58:24 +0200

    write species richness analysis results

--- R/13_Analysis_biodiversity.R
+++ R/13_Analysis_biodiversity.R
@@ -2,15 +2,15 @@
 # A) Species richness ----
 # ----------------------
 m_bd <- stats::glm(
-  species_richness ~ SOC_0_5 * sample_place,
+  species_richness ~ sample_place,
   data = data_bd,
-  family = stats::poisson()
+  family = stats::gaussian()
 )
 
 m_bd_null <- stats::glm(
   SOC_0_5 ~ 1,
   data = data_bd,
-  family = stats::poisson()
+  family = stats::gaussian()
 )
 
 # comparisons & diagnostics
@@ -18,3 +18,24 @@ stats::anova(m_bd, m_bd_null)
 stats::AIC(m_bd, m_bd_null)
 
 stats::qqnorm(stats::resid(m_bd)); stats::qqline(stats::resid(m_bd))
+
+# 4. Tidy results
+tab_bd <- broom.mixed::tidy(m_bd, effects = "fixed", conf.int = TRUE)
+
+# 5. Make regression tables and export
+write_tab_bd <- sjPlot::tab_model(
+  m_bd, m_bd_null,
+  show.icc = TRUE,
+  show.aic = TRUE,
+  show.ci = 0.95,
+  dv.labels = c("Species richness ~ SOC * Habitat", "Null model"),
+  file = "Outputs/Tables/SpeciesRichness_models.doc"
+)
+
+write_tab_bd
+
+# 6. Narrative APA-style summary
+report_bd <- report::report(m_bd)
+
+# Save the textual report
+cat(report_bd, file = "Outputs/Tables/SpeciesRichness_report.txt")
